2d68bbb7d79b2743302de1ad0e786b29df237d31
Lucas Santos <lucashas27@gmail.com>
2022-06-18 20:23:02 -0300

    Add prettier and eslint

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -4,11 +4,7 @@ module.exports = {
     es2021: true,
   },
   extends: [
-    'plugin:react/recommended',
     'eslint:recommended',
-    'plugin:@typescript-eslint/eslint-recommended',
-    'plugin:@typescript-eslint/recommended',
-    'prettier',
   ],
   parserOptions: {
     ecmaFeatures: {
@@ -19,19 +15,10 @@ module.exports = {
   },
   plugins: [
     'react',
-    "@typescript-eslint",
-    "no-loops",
-    "prettier"
   ],
   rules: {
     'linebreak-style': 0,
-    'no-loops/no-loops': 2, // 2 singifica "retornar um errro"
+    'no-loops/no-loops': 2,
     'no-console': 1,
-    'prettier/prettier': 2,
   },
 };
-
-console.log('Hello world!');
-for (let i = 0; i < 12; i++) {
-  console.log(i);
-}
