6c7d41175c0e1ab360965fae6bb5e5d6cafb2f49
Lucas Santos <lucashas27@gmail.com>
2022-06-18 20:12:51 -0300

    Add prettier and eslint

--- client/src/components/ChatPage.jsx
+++ client/src/components/ChatPage.jsx
@@ -1,64 +1,62 @@
-import React from 'react'
-import style from './ChatPage.module.scss'
-import { useEffect, useState } from 'react'
-import io from 'socket.io-client'
-import MessagesArea from './MessagesArea/MessagesArea'
-const socket = io.connect('http://localhost:3001')
+import React, { useEffect, useState } from 'react';
+import io from 'socket.io-client';
+import style from './ChatPage.module.scss';
+import MessagesArea from './MessagesArea/MessagesArea';
 
-const ChatPage = () => {
-  const [message, setMessage] = useState('')
-  const [messageReceived, setMessageReceived] = useState({})
-  const [room, setRoom] = useState('')
-  const [userConnected, setUserConnected] = useState('')
+const socket = io.connect('http://localhost:3001');
+
+function ChatPage() {
+  const [message, setMessage] = useState('');
+  const [messageReceived, setMessageReceived] = useState({});
+  const [room, setRoom] = useState('');
+  const [userConnected, setUserConnected] = useState('');
 
   const joinRoom = () => {
-    if (room !== "") {
-      socket.emit("join_room", room)
+    if (room !== '') {
+      socket.emit('join_room', room);
     }
-  }
+  };
 
   const getUserRoom = () => {
-    let roomFromLocalStorage = localStorage.getItem('userRoom');
-      return roomFromLocalStorage
-  }
+    const roomFromLocalStorage = localStorage.getItem('userRoom');
+    return roomFromLocalStorage;
+  };
 
   const setUserRoom = (userRoom) => {
-
     if (userRoom) {
-      setRoom(userRoom)
+      setRoom(userRoom);
       localStorage.setItem('userRoom', userRoom);
     }
-  }
+  };
 
   const sendMessage = () => {
-    socket.emit('send_message', {message, room}) // envia um evento EMIT do tipo send_message para o backend
+    socket.emit('send_message', { message, room }); // envia um evento EMIT do tipo send_message para o backend
     // tambem envia a room que o usuario esta conectado
-  }
+  };
 
   useEffect(() => {
-    socket.on("receive_message", (data) => { // recebe o evento do tipo receive_message do backend
-
+    socket.on('receive_message', (data) => { // recebe o evento do tipo receive_message do backend
       setMessageReceived(
         {
           message: data.message,
-          room: data.room
-        })
-      
-    })
-    
-    socket.on("receive_connect_message", (data) => {
-      setUserConnected(data.message)
-    })
-  }, [socket]) 
+          room: data.room,
+        },
+      );
+    });
+
+    socket.on('receive_connect_message', (data) => {
+      setUserConnected(data.message);
+    });
+  }, [socket]);
 
   return (
     <div className={style.wrapper}>
-     {messageReceived && (
-      <MessagesArea 
+      {messageReceived && (
+      <MessagesArea
         messageReceived={messageReceived.message}
         roomMessageReceived={messageReceived.room}
       />
-     )}
+      )}
       <div className={style.buttons}>
         <input
           className={style.roomInput}
@@ -69,8 +67,9 @@ const ChatPage = () => {
         />
         <button
           className={style.roomButton}
-          onClick={joinRoom}>
-            Join Room
+          onClick={joinRoom}
+        >
+          Join Room
         </button>
         <input
           className={style.sendMessageInput}
@@ -79,12 +78,15 @@ const ChatPage = () => {
             setMessage(event.target.value);
           }}
         />
-        <button 
+        <button
           className={style.sendMessageButton}
-          onClick={sendMessage}>Send Message</button>
+          onClick={sendMessage}
+        >
+          Send Message
+        </button>
       </div>
     </div>
-  )
+  );
 }
 
-export default ChatPage
+export default ChatPage;
